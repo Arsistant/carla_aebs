86b828ca1dd75bcccf60d54c7674602dfbb2110a
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2019-11-16 12:37:56 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import os
+import argparse
+from scripts.engines.server_manager import ServerManagerBinary
+from scripts.engines.setup_world import SetupWorld
+from scripts.rl_agent.ddpg_agent import ddpgAgent
+from scripts.rl_agent.input_preprocessor import InputPreprocessor
+import numpy as np
+from keras.models import model_from_json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Assurance Monitoring for RL-based emergency braking system.')
+    parser.add_argument("-g", "--gui", help="set gui mode.", action="store_true")
+    parser.add_argument("-t", "--testing", help="set testing mode", action="store_true", default=False)
+    parser.add_argument("-c", "--collect", help="collect the image data")
+    parser.add_argument("-p", "--perception", help="set the path of perception neural network")
+
+    args = parser.parse_args()
+
+    EPISODE = 100
+
+    try:
+        carla_server = ServerManagerBinary({'CARLA_SERVER': os.environ["CARLA_SERVER"]})
+        carla_server.reset()
+        carla_server.wait_until_ready()
+        env = SetupWorld(town=1, gui=args.gui, collect=args.collect, perception=args.perception)
+        agent = ddpgAgent(Testing=args.testing)
+        input_preprocessor = InputPreprocessor()
+        for episode in range(EPISODE):
+            initial_distance = np.random.normal(100, 1)
+            initial_speed = np.random.uniform(26,30)
+            s = env.reset(initial_distance, initial_speed)
+            print("Episode {} is started, target distance: {}, target speed: {}, initial distance: {}, initial speed: {}".format(episode, initial_distance, initial_speed, s[0], s[1]))
+            s = input_preprocessor(s)
+            epsilon = 1.0 - (episode+1)/(EPISODE)
+            while True:
+                a = agent.getAction(s, epsilon)
+                s_, r, done= env.step(a[0][0])
+                s_ = input_preprocessor(s_)
+                if args.testing is False:
+                    agent.storeTrajectory(s, a, r, s_, done)
+                    agent.learn()
+                s = s_
+                if done:
+                    print("Episode {} is done, the reward is {}".format(episode,r))
+                    break
+
+            if args.testing is False:
+                if np.mod(episode, 10) == 0:
+                    agent.save_model()
+        carla_server.stop()
+    
+    except Exception as error:
+        print('Caught this error: ' + repr(error))
+        carla_server.stop()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        carla_server.stop()
---
+++ scripts/engines/distance_calculation.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import cv2
+try:
+    sys.path.append(os.environ["CARLA_PYTHON"])
+    import carla
+    from carla import Image
+except:
+    raise Exception('No CARLA module found.')
+import numpy as np
+from keras.models import model_from_json
+import tensorflow as tf
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+sess = tf.InteractiveSession()
+
+class DistanceCalculation():
+    def __init__(self, ego_vehicle, leading_vehicle, perception=None):
+        self.ego_vehicle = ego_vehicle
+        self.leading_vehicle = leading_vehicle
+        self.perception = perception
+        if perception is not None:
+            with open(os.path.join(perception, "perception_architecture.json"), 'r') as f:
+                self.model = model_from_json(f.read())
+            self.model.load_weights(os.path.join(perception, 'perception_weights.h5'))
+
+
+    def getTrueDistance(self):
+        distance = self.leading_vehicle.get_location().y - self.ego_vehicle.get_location().y \
+                - self.ego_vehicle.bounding_box.extent.x - self.leading_vehicle.bounding_box.extent.x
+        return distance 
+    
+    def getRegressionDistance(self, image):
+        if self.perception is not None:
+            img = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+            img = np.reshape(img, (image.height, image.width, 4))
+            img = cv2.resize(img, (224,224))
+            img = img[:, :, :3]/255.
+            #img = img[:, :, ::-1]/255.
+            img = np.expand_dims(img,axis=0)
+            distance = self.model.predict(img)*120.0
+            return float(distance[0][0])
+        return None
+        
